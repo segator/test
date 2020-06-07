3b6179631f3bc12cdea661df1ac82ea612ad44b7
segator <isaac.aymerich@gmail.com>
2020-06-07 12:42:06 +0200

    no message

--- build.go
+++ build.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	//"transcoder/builder/cmd"
-)
+import "github.com/spf13/cobra/cobra/cmd"
 
 func main() {
-	buildPath := filepath.Join("build","server")
-	fmt.Printf("Create folder %s\n",buildPath)
-	if err:=os.MkdirAll(buildPath,os.ModePerm);err!=nil {
-		panic(err)
-	}
-	//cmd.BuildServer([]string{"windows-amd64","linux-amd64","darwin-amd64"})
-	//cmd.Execute()
+	cmd.Execute()
 }
